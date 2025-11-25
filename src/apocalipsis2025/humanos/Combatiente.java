4f9cd54a1def6438bdfc975267fb20b89af8dcfe
Natalia García Delgado <natalia.garciadelgadowk@gmail.com>
2025-11-25 22:42:46 +0100

    Initial commit

---
+++ src/apocalipsis2025/humanos/Combatiente.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package apocalipsis2025.humanos;
+
+/**
+ *
+ * @author Natalia Garcia
+ */
+public class Combatiente {
+
+}
