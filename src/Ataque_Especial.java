c73b66d38937cc2f4be3aacc78038aa9dd589b91
yoichi3101 <airamfergom2006@gmail.com>
2025-12-26 00:29:55 +0100

    Clase juego (Completa o casi completa)

--- src/Ataque_Especial.java
+++ src/Ataque_Especial.java
@@ -1,5 +1,3 @@
-package juego;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,8 +5,8 @@ package juego;
 
 /**
  *
- * @author iquli
+ * @author Maria
  */
-public enum Estado {
-    ACTIVO,ELIMINADO
+class Ataque_Especial {
+    
 }
